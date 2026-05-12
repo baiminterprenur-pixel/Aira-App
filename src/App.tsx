52c0329791e9e865ac16362db9dc328031aa8232
baiminterprenur-pixel <baiminterprenur@gmail.com>
2026-05-12 21:06:43 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Aira App Berhasil Jalan 🚀</h1>
+      <p>Deploy Vercel berhasil.</p>
+    </div>
+  )
+}
